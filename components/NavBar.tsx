cb06ecb7d7074c6f2a78c3be7231054880d3ee5e
alidauda <alidauda14@gmail.com>
2023-08-30 22:49:16 +0100

    added add tocart and did some small change

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { Suspense } from 'react';
+import OpenCart from './cart/open-cart';
 
 export default async function Navbar() {
   return (
@@ -19,7 +20,7 @@ export default async function Navbar() {
           <ul className='hidden gap-6 text-sm md:flex md:items-center'>
             <li>
               <Link
-                href={'#'}
+                href={'/'}
                 className='text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300'
               >
                 Home
@@ -28,7 +29,12 @@ export default async function Navbar() {
           </ul>
         </div>
         <div className='hidden justify-center md:flex md:w-1/3'></div>
-        <div className='flex justify-end md:w-1/3'></div>
+        <div className='flex justify-end md:w-1/3'>
+          wsjhsjs
+          <Suspense fallback={<OpenCart />}>
+            <Cart />
+          </Suspense>
+        </div>
       </div>
     </nav>
   );
